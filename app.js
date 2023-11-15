6e3f9ee8a3ca44653803ba1cc181ad3093972023
NahuelPalladini <palladininahuel@gmail.com>
2023-11-15 12:04:09 -0300

    Transiciones

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const PORT = 3030;
 /* RUTES */
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+app.get('/contac', (req, res) => res.sendFile(path.join(__dirname, 'views', 'contac.html')));
+
 
 /* SERVER */
 app.listen(PORT, () => console.log(`Server Running in http://localhost:${PORT}`));
